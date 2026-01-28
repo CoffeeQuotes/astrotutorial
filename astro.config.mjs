04c778f4ea00eaad72bd19d90098eddec4828d02
CoffeeQuotes <sky8052785942ocean@gmail.com>
2026-01-28 17:59:41 +0530

    content collection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://mellow-moonbeam-69bab9.netlify.app/",
+  site: "https://mellow-moonbeam-69bab9.netlify.app/",
+  integrations: [preact()],
 });
